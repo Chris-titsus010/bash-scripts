4a6eefc938b69e6c054de9eea9766b6e3a8382b0
Jessy Varin <jessy.varin@epitech.eu>
2022-03-23 12:00:32 +0100

    tmp removed

--- disable_proxy.sh
+++
@@ -1,6 +0,0 @@
-#! bin/bash
-
-#escaped_proxy=$(printf '%s\n' "&~\"#'{([-|_\\@)]=+}^\$*%!:;.,?/<>" | sed -re "s/([\/&^$.*+?\"(){}])/\\\\\1/g")
-#echo -e "$escaped_proxy\n"
-sed "s/\&~\"#'{\(\[\-\|_\\@\)\]=\+}\^\$\*%!\:;\.,\?\/<>//g" "/home/jessy/test2.txt"
-
