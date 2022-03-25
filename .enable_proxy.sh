be6ca348de026c7071f5623b6c57077c8a7c6fe8
jessy <jessy.varin@epitech.eu>
2022-03-25 22:36:54 +0100

    using regex to add values instead of appending whole list of variable to the file

--- .enable_proxy.sh
+++ .enable_proxy.sh
@@ -1,7 +1,5 @@
 #! bin/bash
 
-sudo tee -a /etc/environment > /dev/null <<EOT
-http_proxy=$(sudo -i printenv http_proxy)
-https_proxy=$(sudo -i printenv https_proxy)
-ftp_proxy=$(sudo -i printenv ftp_proxy)
-EOT
+sed -i -r "s/((https?|ftp)_proxy)=\"\"/\1=\"$(sudo -i printenv http_proxy)\"/g" "test2.txt"
+
+exit 0
