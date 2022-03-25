2b7857a3ff30650db3013a40a6ffbac1f6d97cd2
jessy <jessy.varin@epitech.eu>
2022-03-25 23:06:44 +0100

    playing sound from url using google translate

---
+++ playOnStroke/playOnStroke.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+play -t mp3 'https://www.youtube.com/watch?v=pjJ2w1FX_Wg'
+
+http://translate.google.com/translate_tts\?ie\=UTF-8\&client\=tw-ob\&tl\=ru\&q=%D0%AF%20%D1%81%D0%BA%D0%B0%D0%B7%D0%B0%D0%BB
